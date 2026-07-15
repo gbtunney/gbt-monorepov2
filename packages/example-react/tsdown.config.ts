6fb5b4ee03ccd3a0a111c4b5316cbd51fc17d77b
Gillian Tunney <gbtunney@mac.com>
2026-07-15 19:34:45 -0400

    fix(root,template-example-react): annoying, stupid

--- packages/example-react/tsdown.config.ts
+++ packages/example-react/tsdown.config.ts
@@ -1,23 +1,8 @@
-import { defineBuildPlan, toTsdownConfigs } from '@snailicid3/build-config'
 import { defineConfig } from 'tsdown'
-import pkg from './package.json' with { type: 'json' }
 
-const plan = defineBuildPlan(pkg, {
-    entries: [
-        {
-            banner: true,
-            key: '*',
-            lint: false,
-            output_formats: ['esm', 'cjs', 'iife', 'ts'],
-            runtime: 'node',
-        },
-    ],
-    root: {
-        outputDir: './dist',
-        sourceDir: './src',
-    },
+export default defineConfig({
+  platform: 'neutral',
+  dts: true,
+  exports: true,
+  // ...config options
 })
-
-const tsdownConfigs = toTsdownConfigs(plan)
-
-export default defineConfig(tsdownConfigs)
