5cc10787d4f9a68cb7079aef9c32e52b7929b87b
Gillian Tunney <gbtunney@mac.com>
2026-07-16 03:37:07 +0000

    fix(template-example-react): fixed chromatic

--- packages/example-react/src/stories/Button.stories.ts
+++ packages/example-react/src/stories/Button.stories.ts
@@ -27,7 +27,7 @@ type Story = StoryObj<typeof meta>
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const primary: Story = {
     args: {
-        backgroundColor: 'purple',
+        backgroundColor: 'blue',
         label: 'Button',
         primary: true,
     },
