da9456e2a72c45ef3c07cbc79cb78d591e995462
Gillian Tunney <gbtunney@mac.com>
2026-07-16 07:48:06 -0400

    fix(root): temporary fix for import-x and other

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,7 @@
 import { EsLint } from '@snailicid3/config'
-const CONFIG = EsLint.config({ cwd: import.meta })
+
+const CONFIG = EsLint.config({
+    cwd: import.meta,
+    overrides: [{ settings: { 'import-x/ignore': ['node_modules'] } }],
+})
 export default EsLint.defineConfig(CONFIG)
